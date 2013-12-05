6173083c5de3bfc24b5a4d5a2063e6cf926f22f3
furugomu <furugomu@gmail.com>
2013-12-05 19:19:58 +0900

    編集できるようになった

--- public/hoge.js
+++ public/hoge.js
@@ -2,21 +2,32 @@ var app = angular.module("unko", []);
 app.controller('Ctrl', function($scope, $http) {
   $scope.search = {type: ""};
   $scope.selected = {};
-  $scope.scenario = {paragraphs: []};
+  $scope.paragraphs = [];
 
   $http.get('millionstars.json').success(function(idols) {
     $scope.idols = idols;
 
     $scope.selected.idol = idols[37];
-    $scope.selected.index = 0;
+    $scope.selected.imageIndex = 0;
   });
 
   $scope.addParagraph = function() {
-    $scope.scenario.paragraphs.push({
-      idol: $scope.selected.idol,
-      image: $scope.selected.idol.images[$scope.selected.index],
-      serifu: $scope.serifu,
-      imageIndex: $scope.selected.index,
-    });
+    $scope.paragraphs.push(angular.copy($scope.selected));
+    $scope.selected.text = '';
   }
+
+  var swap = function(array, i, j) {
+    var t = array[i];
+    array[i] = array[j];
+    array[j] = t;
+  }
+
+  $scope.moveup = function(array, i) {
+    swap(array, i, i-1);
+  }
+
+  $scope.movedown = function(array, i) {
+    swap(array, i, i+1);
+  }
+
 });
